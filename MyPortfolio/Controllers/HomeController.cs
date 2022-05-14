9cf334981836cdb7086cd4a81a65496ca96db155
даша Даша <DaridaKr@gmail.com>
2022-05-14 18:26:21 +0300

    Made functional culture dropdown

--- MyPortfolio/Controllers/HomeController.cs
+++ MyPortfolio/Controllers/HomeController.cs
@@ -25,14 +25,14 @@ namespace MyPortfolio.Controllers
         }
 
         [HttpPost]
-        public IActionResult CultureManagement(string culture)
+        public IActionResult CultureManagement(string culture, string returnUrl)
         {
             // Set the cookies.
             Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
                 CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
                 new CookieOptions { Expires = DateTimeOffset.Now.AddDays(30) });
 
-            return RedirectToAction(nameof(Index));
+            return LocalRedirect(returnUrl);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
